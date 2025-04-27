da8dc8a4c1847aafe6a729eaf0ffe813a436cb84
Akamiz96 <alejandro.castro2016@outlook.es>
2025-04-27 15:59:18 -0500

    fix(scripts): Fix execution of exp0 shell scripts

--- elasticity-m2/deployment/cleanup.sh
+++ elasticity-m2/deployment/cleanup.sh
@@ -11,16 +11,27 @@
 set -euo pipefail
 
 # Variables
-MANIFESTS_DIR="manifests"
+MANIFESTS_DIR="./deployment/manifests"
 
 echo "========================================="
 echo " INICIO DE LA LIMPIEZA DE RECURSOS"
 echo "========================================="
 
-# Borrar deployments, services, configmaps
+# 1. Eliminar deployments, services, configmaps
 echo "Eliminando manifiestos de Kubernetes..."
 kubectl delete -f "$MANIFESTS_DIR/" --ignore-not-found
+
+# 2. Esperar unos segundos
+sleep 3
+
+# 3. Forzar eliminación de pods que queden en Terminating
+echo "Forzando eliminación de pods restantes..."
+kubectl get pods --no-headers=true | awk '/Terminating/ {print $1}' | while read pod; do
+    echo "Forzando eliminación de pod $pod..."
+    kubectl delete pod "$pod" --grace-period=0 --force
+done
 
 echo "========================================="
 echo " LIMPIEZA COMPLETADA"
 echo "========================================="
+
